d4e18b699cfd4bf7923ad54c30e6abe29ad7a44a
Lauren Hamel <lauren.hamel@outlook.com>
2020-03-22 23:08:17 -0400

    Improves handling of loading

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ require('dotenv').config();
 // Load dependencies.
 const _ = require('lodash');
 const fs = require('fs-extra');
+const chalk = require('chalk');
 const { program } = require('commander');
 const capture = require('capture-website');
 const MapTiler = new (require('./lib/MapTiler'))();
+const ProgressBar = require('cli-progress');
 
 // Load package data.
 const pkg = require('./package.json');
@@ -17,27 +19,179 @@ const pkg = require('./package.json');
 program.version(pkg.version);
 
 // Define program options.
-program.option('-l, --location <location>', 'The map location', async (dest) => {
+program.option('-l, --location <location>', 'The map location');
+
+// Parse program agurments.
+program.parse(process.argv);
+
+// Run program asynchronously.
+(async () => {
+
+  // Configure the program.
+  const config = {
+    lat: {
+      offset: .03
+    },
+    lng: {
+      offset: .06
+    }
+  };
 
   // Get data for the destination.
-  const data = await MapTiler.MapData(dest);
+  const data = await MapTiler.MapData(program.location);
 
-  // Get the URL for the map.
-  const url = MapTiler.MapURL(...data.coords);
+  // Output a message.
+  console.log(`Capturing map of ${chalk.cyan(data.geocoding.place_name)}...`);
 
-  // Generate image file name.
-  const filename = data.coords.map((coord) => _.replace(coord, '.', ',')).join('x');
+  // Calculate minimum and maximum coordinates to capture.
+  config.lat.min = data.bounds.SW[0];
+  config.lat.max = data.bounds.NE[0];
+  config.lat.diff = Math.abs(config.lat.max - config.lat.min);
+  config.lat.steps = Math.ceil(config.lat.diff / config.lat.offset);
+  config.lng.min = data.bounds.SW[1];
+  config.lng.max = data.bounds.NE[1];
+  config.lng.diff = Math.abs(config.lng.max - config.lng.min);
+  config.lng.steps = Math.ceil(config.lng.diff / config.lng.offset);
 
-  // Ensure the temporary folder is in place.
-  fs.ensureDirSync('tmp/');
+  // Generate an empty matrix for the image coordinates.
+  let matrix = new Array(config.lat.steps).fill(null).map(() => ([
+    ...new Array(config.lng.steps).fill(null)
+  ]));
 
-  // Take a screenshot of the map and save it to an image.
-  const img = await capture.file(url, `tmp/${filename}.png`, {
-    overwrite: true,
-    hideElements: ['.mapboxgl-control-container']
+  // Fill the matrix with the range of coordinates.
+  matrix = matrix.map((lat, x) => {
+
+    // Calcualte the latitude coordinate.
+    const _lat = config.lat.min + (x * config.lat.offset);
+
+    // Return the calculated coordinates.
+    return lat.map((lng, y) => {
+
+      // Calculate the longitude coordinate.
+      const _lng = config.lng.min + (y * config.lng.offset);
+
+      // Return the new coordinates.
+      return [_lat, _lng];
+
+    });
+
   });
 
-});
+  // Get matrix dimensions.
+  const grid = {
+    x: config.lat.steps,
+    y: config.lng.steps,
+    n: config.lat.steps * config.lng.steps,
+    i: 0
+  };
 
-// Parse agurments.
-program.parse(process.argv);
+  // Output a message.
+  console.log(`Map area will be rendered as a ${chalk.magenta(grid.x)} x ${chalk.magenta(grid.y)} grid.`);
+
+  // Initialize a progress bar.
+  const progress = new ProgressBar.SingleBar({}, ProgressBar.Presets.shades_classic);
+
+  // Start the progress bar.
+  progress.start(grid.n);
+
+  // Ensure the images folder is in place.
+  fs.ensureDirSync('images/');
+
+  // Build an array of request data.
+  const requests = [];
+
+  // Loop through the range of coordinates and grab screenshots.
+  matrix.forEach((row, x) => {
+
+    // Capture each column within the row.
+    row.forEach((col, y) => {
+
+      // Get the map URL for the coordinate.
+      const url = MapTiler.MapURL(...col);
+
+      // Generate an image file name.
+      const filename = `${x + 1}x${y + 1}_${col.map((coord) => _.replace(col, '.', ',')).join('x')}`;
+
+      // Add screen grabs to the queue.
+      requests.push({
+        url,
+        path: `images/${filename}.png`,
+        coords: col
+      });
+
+    });
+
+  });
+
+  // Capture statuses of images.
+  const images = [];
+  const success = [];
+  const errors = [];
+
+  // Process each request.
+  for( let i = 0; i < requests.length; i++ ) {
+
+    // Capture data about the request.
+    const { url, path, coords } = requests[i];
+
+    // Take a screenshot of the map and save it to an image.
+    try {
+
+      // Take a screenshot of the map image.
+      const img = await capture.file(url, path, {
+        overwrite: true,
+        hideElements: ['.mapboxgl-control-container'],
+        delay: .5
+      });
+
+      // Capture data about the saved image.
+      success.push({
+        coords,
+        path
+      });
+
+      // Save the image.
+      images.push(img);
+
+      // Increment the grid step.
+      grid.i++;
+
+      // Update the progress bar.
+      progress.increment();
+
+    } catch (error) {
+
+      // Capture any errors.
+      errors.push({
+        coords,
+        path,
+        error
+      });
+
+    }
+
+  }
+
+  // Stop the progress bar.
+  progress.stop();
+
+  // Determine if errors occurred.
+  if(errors.length) {
+
+    // Output a message.
+    console.log(chalk.red(`✓ Map images saved, but ${errors.length} errors occurred.`));
+
+    // Output errors.
+    errors.forEach((error) => console.log(error));
+
+  }
+
+  // Otherwise, everything worked.
+  else {
+
+    // Output a message.
+    console.log(chalk.green(`✓ Map images saved successfully.`));
+
+  }
+
+})();
