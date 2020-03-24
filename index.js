22ac8e5619ed9a46c83da1361d1a495bc88ec954
Lauren Hamel <lauren.hamel@outlook.com>
2020-03-24 15:22:03 -0400

    Adds support for passing in coordinates to use and adds subdirectory structure to generated files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// TODO: Add support for passing SW+NE boundary coordinates instead of location.
+
 // Load environment variables.
 require('dotenv').config();
 
@@ -20,13 +22,12 @@ const pkg = require('./package.json');
 // Set program version.
 program.version(pkg.version);
 
-// Define program options.
+// Define program options, and run it.
 program
   .option('-l, --location <location>', 'The map location')
-  .option('-t, --tile <number>', 'The tile number within the grid to regenerate');
-
-// Parse program agurments.
-program.parse(process.argv);
+  .option('-c, --coordinates <coordinates>', 'The SW and NE coordinates for the map area, given as `[SW Latitude, SW Longitude] [NE Latitude, NE Longitude]`')
+  .option('-t, --tile <number>', 'The tile number within the grid to regenerate')
+  .parse(process.argv);
 
 // Run program asynchronously.
 (async () => {
@@ -44,16 +45,48 @@ program.parse(process.argv);
   // Get data for the destination.
   const data = await MapTiler.MapData(program.location);
 
+  // Get the place name.
+  const place = {
+    long: data.geocoding.place_name,
+    short: data.geocoding.text
+  };
+
   // Output a message.
-  console.log(`Capturing map of ${chalk.cyan(data.geocoding.place_name)}...`);
+  console.log(`Capturing map of ${chalk.cyan(place.long)}...`);
+
+  // Define coordinates.
+  const coords = {
+    SW: data.bounds.SW,
+    NE: data.bounds.NE
+  };
+
+  // Parse and override coordinates if they were given.
+  if(program.coordinates) {
+
+    // Initialize a regex to help validate coordinates.
+    const regex = /^(\[(\-?\d+(?:\.\d+)?)\, ?(\-?\d+(?:\.\d+)?)\]) (\[(\-?\d+(?:\.\d+)?)\, ?(\-?\d+(?:\.\d+)?)\])$/;
+
+    // Initialize empty match set.
+    let matches;
+
+    // Validate that valid coordinates were given.
+    if((matches = program.coordinates.trim().match(regex))) {
+
+      // Capture the SW and NE coordinates.
+      coords.SW = [+matches[2], +matches[3]];
+      coords.NE = [+matches[5], +matches[6]];
+
+    }
+
+  }
 
   // Calculate minimum and maximum coordinates to capture.
-  config.lat.min = data.bounds.SW[0];
-  config.lat.max = data.bounds.NE[0];
+  config.lat.min = coords.SW[0];
+  config.lat.max = coords.NE[0];
   config.lat.diff = Math.abs(config.lat.max - config.lat.min);
   config.lat.steps = Math.ceil(config.lat.diff / config.lat.offset);
-  config.lng.min = data.bounds.SW[1];
-  config.lng.max = data.bounds.NE[1];
+  config.lng.min = coords.SW[1];
+  config.lng.max = coords.NE[1];
   config.lng.diff = Math.abs(config.lng.max - config.lng.min);
   config.lng.steps = Math.ceil(config.lng.diff / config.lng.offset);
 
@@ -78,7 +111,7 @@ program.parse(process.argv);
   };
 
   // Output a message.
-  console.log(`Map area will be rendered as a ${chalk.magenta(grid.x)} x ${chalk.magenta(grid.y)} grid.`);
+  console.log(`Map area will be rendered as a ${chalk.magenta(grid.x)} x ${chalk.magenta(grid.y)} grid (${chalk.magenta(grid.x * grid.y)} total tiles).`);
 
   // Initialize a progress bar.
   const progress = new ProgressBar.SingleBar({}, ProgressBar.Presets.shades_classic);
@@ -86,8 +119,15 @@ program.parse(process.argv);
   // Start the progress bar.
   progress.start(program.tile ? 1 : grid.n);
 
+  // Get coordinates as strings.
+  coords.SW.toString = () => coords.SW.map((coord) => _.replace(coord, '.', ',')).join('x');
+  coords.NE.toString = () => coords.NE.map((coord) => _.replace(coord, '.', ',')).join('x');
+
+  // Create a directory for saving the images.
+  const dir = `images/${place.short}_${coords.SW.toString()}_${coords.NE.toString()}`;
+
   // Ensure the images folder is in place.
-  fs.ensureDirSync('images/');
+  fs.ensureDirSync(dir);
 
   // Build an array of request data.
   const requests = [];
@@ -113,7 +153,7 @@ program.parse(process.argv);
         // Add screen grabs to the queue.
         queue.push({
           url,
-          path: `images/${filename}.png`,
+          path: `${dir}/${filename}.png`,
           coords: col
         });
 
@@ -127,7 +167,7 @@ program.parse(process.argv);
       // Add screen grabs to the queue.
       queue.push({
         url,
-        path: `images/${filename}.png`,
+        path: `${dir}/${filename}.png`,
         coords: col
       });
 
